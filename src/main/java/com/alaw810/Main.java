f5a1793b2246939ef6a9e75c06ce7ffec4c3465b
Adrià Lorente <adria810@gmail.com>
2025-07-12 17:24:04 +0200

    feat: create Restaurant class with custom equality logic

---
+++ src/main/java/com/alaw810/Main.java
@@ -0,0 +1,4 @@
+package com.alaw810;
+
+public class Main {
+}
---
+++ src/main/java/com/alaw810/Restaurant.java
@@ -0,0 +1,38 @@
+package com.alaw810;
+
+public class Restaurant {
+    private String name;
+    private int rating;
+
+    public Restaurant(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant name: " + name +
+                ", rating: " + rating;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Restaurant other = (Restaurant) obj;
+        return this.rating == other.rating && this.name.equalsIgnoreCase(other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.toLowerCase().hashCode() + Integer.hashCode(rating);
+    }
+}
